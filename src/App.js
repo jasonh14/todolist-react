4817fdf1a95e79be1fbd1b6c9fbf56548b2b831c
Jason Hartanto <jason.hartanto@binus.ac.id>
2022-03-03 21:55:57 +0700

    add reminder

--- src/App.js
+++ src/App.js
@@ -25,13 +25,21 @@ function App() {
   ]);
 
   const deleteTask = (id) => {
-    console.log("delete", id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    console.log(id);
   };
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        <p>No Task To Show</p>
+      )}
     </div>
   );
 }
