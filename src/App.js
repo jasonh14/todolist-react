4689bcb76f907f1dc83ada3f302b43fc3d46e755
Jason Hartanto <jason.hartanto@binus.ac.id>
2022-03-04 22:03:26 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ function App() {
     return data;
   };
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
   const deleteTask = async (id) => {
     await fetch(`http://localhost:5000/tasks/${id}`, {
       method: "DELETE",
@@ -31,10 +37,23 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const dataToToggle = await fetchTask(id);
+    const updData = {...dataToToggle, reminder: !dataToToggle.reminder};
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updData),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? {...task, reminder: !task.reminder} : task
+        task.id === id ? {...task, reminder: data.reminder} : task
       )
     );
   };
